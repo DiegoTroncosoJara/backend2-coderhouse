7e40ad795f84570b42f7a2e5b84b5af970f99f69
DiegoTroncosoJara <diego.troncosoojara@gmail.com>
2025-03-17 22:09:48 -0300

    dto product

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,9 +1,13 @@
-import { Router } from "express";
-import { productController } from "../controllers/product.controller.js";
-import { productValidator } from "../middlewares/product.validator.js";
+import { Router } from 'express'
+import { productController } from '../controllers/product.controller.js'
+import { productValidator } from '../middlewares/product.validator.js'
 
 const router = Router()
 
-router.post('/', [productValidator], productController.create);
+router.post('/', [productValidator], productController.create)
 
+router.get('/', productController.getAll)
+
+//APLICA DTO
+router.get('/:id', productController.getProdById)
 export default router
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,17 +1,18 @@
-import { Router } from "express";
-import {
-  userController,
-} from "../controllers/user.controller.js";
-import { passportCall } from "../passport/passportCall.js";
-// import { roleAuth } from '../middlewares/roleAuth.js'
+import { Router } from 'express'
+import { userController } from '../controllers/user.controller.js'
+import { passportCall } from '../passport/passportCall.js'
+import { roleAuth } from '../middlewares/roleAuth.js'
 
-const router = Router();
+const router = Router()
 
-router.post("/register", userController.register);
+router.post('/register', userController.register)
 
-router.post("/login", userController.login);
+router.post('/login', userController.login)
 
-router.get("/current", [passportCall('current')], userController.privateData);
+router.get(
+  '/current',
+  [passportCall('current'), roleAuth('admin')],
+  userController.privateData
+)
 
-
-export default router;
+export default router
